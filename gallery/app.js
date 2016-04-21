84bb193242d6af6bd1d04bc92aea68694a2970f7
Stian Prestholdt <stian@e5r.no>
2016-04-21 17:58:16 +0200

    Add node gallery

---
+++ gallery/app.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+var app = express();
+
+app.use('/', require('node-gallery')({
+  staticFiles : '../categories',
+  urlRoot : 'gallery',
+  title : 'Example Gallery'
+}));
+
+app.listen(3012, '0.0.0.0');
