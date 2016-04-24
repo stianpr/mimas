c4849ee3426b82b2e5d21dd857dc51284e31dd2e
Stian Prestholdt <stian@e5r.no>
2016-04-24 10:50:34 +0200

    Add format for speed on windy

--- sensors/windy.py
+++ sensors/windy.py
@@ -7,4 +7,4 @@ from lib.QSFS import Sensor
 with Sensor(gpio=18, record_time=0.5) as sensor:
     speed, rpm, count = sensor.get_readings()
 
-print('{:.2f} / {:.0f}rpm / {:.0f}Hz'.format(speed, rpm, count))
+print('{:.2f}m/s / {:.0f}rpm / {:.0f}Hz'.format(speed, rpm, count))
