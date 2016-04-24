e121f10e968e6d33693805df9eb4b1ee11f5e9b2
Stian Prestholdt <stian@e5r.no>
2016-04-24 10:31:05 +0200

    Fix correct output for windy

--- sensors/windy.py
+++ sensors/windy.py
@@ -7,4 +7,4 @@ from lib.QSFS import Sensor
 with Sensor(gpio=18, record_time=0.2) as sensor:
     speed, rpm, count = sensor.get_readings()
 
-print('{:.2f} / {:.0f}Hz / {:.0f}rpm'.format(speed, rpm, count))
+print('{:.2f} / {:.0f}rpm / {:.0f}Hz'.format(speed, rpm, count))
