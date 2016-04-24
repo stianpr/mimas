07b0ae80c49a91cc491e349da661f3f3b9f3716d
Stian Prestholdt <stian@e5r.no>
2016-04-24 10:46:17 +0200

    Update reading time for windy sensor

--- sensors/windy.py
+++ sensors/windy.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from lib.QSFS import Sensor
 
 
-with Sensor(gpio=18, record_time=0.2) as sensor:
+with Sensor(gpio=18, record_time=0.5) as sensor:
     speed, rpm, count = sensor.get_readings()
 
 print('{:.2f} / {:.0f}rpm / {:.0f}Hz'.format(speed, rpm, count))
