bc29125ac1c84f3af0a8f83233979c5f71c3f5f5
Stian Prestholdt <stian@e5r.no>
2016-05-08 19:09:24 +0200

    Fix correct import of modules in daemon files

--- src/daemons/humy.py
+++ src/daemons/humy.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-sys.path.append(os.path.abspath('../'))
+filepath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.realpath(filepath + '/..'))
 
 from daemons.daemon import Daemon
 from database.models import SensorHumidity
