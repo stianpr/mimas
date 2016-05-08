4823bea6366f5d2d1ee13b9dc455ba8fc2f00791
Stian Prestholdt <stian@e5r.no>
2016-05-08 18:47:22 +0200

    Add reading date with timezone in SQL schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,52 +1,52 @@
 CREATE TABLE sensors_pressure (
-  id           SERIAL    NOT NULL,
-  reading_time timestamp NOT NULL DEFAULT now(),
-  pressure     decimal   NOT NULL,
-  temperature  decimal   NOT NULL,
-  altitude     decimal   NOT NULL,
+  id           SERIAL                   NOT NULL,
+  reading_time timestamp with time zone NOT NULL DEFAULT now(),
+  pressure     decimal                  NOT NULL,
+  temperature  decimal                  NOT NULL,
+  altitude     decimal                  NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE sensors_humidity (
-  id           SERIAL    NOT NULL,
-  reading_time timestamp NOT NULL DEFAULT now(),
-  humidity     decimal   NOT NULL,
-  temperature  decimal   NOT NULL,
+  id           SERIAL                   NOT NULL,
+  reading_time timestamp with time zone NOT NULL DEFAULT now(),
+  humidity     decimal                  NOT NULL,
+  temperature  decimal                  NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE sensors_temperature (
-  id           SERIAL    NOT NULL,
-  reading_time timestamp NOT NULL DEFAULT now(),
+  id           SERIAL                   NOT NULL,
+  reading_time timestamp with time zone NOT NULL DEFAULT now(),
   temperature  decimal   NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE sensors_wind (
-  id           SERIAL    NOT NULL,
-  reading_time timestamp NOT NULL DEFAULT now(),
-  speed        decimal   NOT NULL,
-  rpm          integer   NOT NULL,
-  hertz        integer   NOT NULL,
+  id           SERIAL                    NOT NULL,
+  reading_time timestamp with time zone  NOT NULL DEFAULT now(),
+  speed        decimal                   NOT NULL,
+  rpm          integer                   NOT NULL,
+  hertz        integer                   NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE sensors_precipitation (
-  id           SERIAL    NOT NULL,
-  reading_time timestamp NOT NULL DEFAULT now(),
-  total        decimal   NOT NULL,
+  id           SERIAL                    NOT NULL,
+  reading_time timestamp with time zone  NOT NULL DEFAULT now(),
+  total        decimal                   NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE weather_log (
-  id             SERIAL    NOT NULL,
-  reading_time   timestamp NOT NULL DEFAULT now(),
-  temperature    decimal   NOT NULL,
-  pressure       decimal   NOT NULL,
-  humidity       decimal   NOT NULL,
-  precipitation  decimal   NOT NULL,
-  wind_avg       decimal   NOT NULL,
-  wind_gust      decimal   NOT NULL,
-  wind_direction integer   NOT NULL,
+  id             SERIAL                    NOT NULL,
+  reading_time   timestamp with time zone  NOT NULL DEFAULT now(),
+  temperature    decimal                   NOT NULL,
+  pressure       decimal                   NOT NULL,
+  humidity       decimal                   NOT NULL,
+  precipitation  decimal                   NOT NULL,
+  wind_avg       decimal                   NOT NULL,
+  wind_gust      decimal                   NOT NULL,
+  wind_direction integer                   NOT NULL,
   PRIMARY KEY (id)
 );
