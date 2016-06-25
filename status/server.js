50453de8d33e62205c855490c3d993004de29490
Stian Prestholdt <stian@e5r.no>
2016-06-25 16:30:40 +0200

    Simpify websocket server

--- status/server.js
+++ status/server.js
@@ -1,7 +1,6 @@
 var WebSocketServer = require('websocket').server;
 var express = require('express')
 var http = require('http');
-
 var app = express();
 var server = http.createServer(app);
 var pg = require('pg').native;
@@ -26,22 +25,8 @@ var wsServer = new WebSocketServer({
     httpServer: server,
 });
 
-var connections = [];
-var data = {
-    temperature: null,
-    pressure: null,
-    humidity: null,
-    rain: null,
-    wind: {
-        avg: null,
-        gust: null,
-        direction: null,
-    },
-};
-
 wsServer.on('request', function (request) {
   var connection = request.accept('sensor-status', request.origin);
-  connections.push(connection);
 
   pg.connect(connString, function (error, client) {
     if (error) {
@@ -49,126 +34,19 @@ wsServer.on('request', function (request) {
     }
 
     client.on('notification', function (msg) {
-      var parts = msg.split(',');
-      var tableName = parts[0];
-      var id = parts[2];
+      var parts = msg.payload.split(':');
+      var data = {
+        type: parts[0],
+        value: parts[1],
+      };
 
-      //update_data(tableName, id);
-      console.log(msg);
+      console.log(type, data);
 
-      //connection.sendUTF(JSON.stringify({data: data}));
+      connection.sendUTF(JSON.stringify({data: data}));
     });
 
     client.query("LISTEN sensor_change");
   });
-
-  connection.on('close', function () {
-    console.log(connection.remoteAddress + ' disconnected');
-
-    // Remove connections from the pool
-    var index = connections.indexOf(connection);
-    if (index !== -1) {
-      connections.splice(index, 1);
-    }
-  });
 });
 
 console.log('Status app ready');
-
-
-// setInterval(function () {
-//     pg.connect(connString, function (err, client, done) {
-//       if (!err) {
-//           client.query(
-//             'SELECT temperature FROM sensors_temperature ' +
-//             'ORDER BY reading_time DESC LIMIT 1',
-//                 function (err, result) {
-//                     done();
-//
-//                     if (!err) {
-//                         data.temperature = result.rows[0].temperature;
-//                     }
-//                 }
-//             );
-//       }
-//     });
-//
-//     pg.connect(connString, function (err, client, done) {
-//       if (!err) {
-//           client.query(
-//             'SELECT humidity FROM sensors_humidity ' +
-//             'ORDER BY reading_time DESC LIMIT 1',
-//                 function (err, result) {
-//                     done();
-//
-//                     if (!err) {
-//                         data.humidity = result.rows[0].humidity;
-//                     }
-//                 }
-//             );
-//       }
-//     });
-//
-//     pg.connect(connString, function (err, client, done) {
-//       if (!err) {
-//           client.query(
-//             'SELECT pressure FROM sensors_pressure ' +
-//             'ORDER BY reading_time DESC LIMIT 1',
-//                 function (err, result) {
-//                     done();
-//
-//                     if (!err) {
-//                         data.pressure = result.rows[0].pressure;
-//                     }
-//                 }
-//             );
-//       }
-//     });
-//
-//     pg.connect(connString, function (err, client, done) {
-//       if (!err) {
-//           client.query(
-//             'SELECT sum(total) as total FROM sensors_precipitation',
-//                 function (err, result) {
-//                     done();
-//
-//                     if (!err) {
-//                         data.rain = result.rows[0].total;
-//                     }
-//                 }
-//             );
-//       }
-//     });
-//
-//     pg.connect(connString, function (err, client, done) {
-//       if (!err) {
-//           client.query(
-//             'SELECT speed FROM sensors_wind ORDER BY speed DESC LIMIT 1',
-//                 function (err, result) {
-//                     done();
-//
-//                     if (!err) {
-//                         data.wind.gust = result.rows[0].speed;
-//                     }
-//                 }
-//             );
-//       }
-//     });
-//
-//     pg.connect(connString, function (err, client, done) {
-//       if (!err) {
-//           client.query(
-//               'SELECT speed FROM sensors_wind ' +
-//               'ORDER BY reading_time DESC LIMIT 1',
-//                 function (err, result) {
-//                     done();
-//
-//                     if (!err) {
-//                         data.wind.avg = result.rows[0].speed;
-//                     }
-//                 }
-//             );
-//       }
-//     });
-//
-// }, 2000);
