efd395693971596d6c26d0e2acbf5e6a79973aef
Stian Prestholdt <stian@e5r.no>
2016-06-26 21:52:39 +0200

    Fix better error handling for direction

--- sensors/QSFX.py
+++ sensors/QSFX.py
@@ -31,15 +31,12 @@ class Sensor(object):
 
     def get_value(self):
         """Return SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)"""
-        if self.channel > 7 or self.channel < 0:
-            return -1
-
         r = self.spi.xfer2([1, 8 + self.channel << 4, 0])
         return ((r[1] & 3) << 8) + r[2]
 
     def get_readings(self):
         value = self.get_value()
         volts = (value * self.volts) / 1024
-        direction = (volts - 0.4) / 16 * 360
+        direction = (volts - 0.4) / 16 * 360 if volts > 0 else 0
 
-        return SensorDirection(direction=direction)
+        return SensorDirection(direction=direction * 10)
