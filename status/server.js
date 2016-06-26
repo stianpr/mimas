dda1ed2c416203afc57ed087a5c266e439ff6cf5
Stian Prestholdt <stian@e5r.no>
2016-06-26 18:05:35 +0200

    Improve sensor server with pushing only changes

--- status/server.js
+++ status/server.js
@@ -1,21 +1,9 @@
+var EventEmitter = require('event-emitter');
 var WebSocketServer = require('websocket').server;
-var express = require('express')
 var http = require('http');
-var app = express();
-var server = http.createServer(app);
+var server = http.createServer();
 var pg = require('pg').native;
 var connString = 'postgres:///mimas';
-
-
-app.use(express.static(__dirname + "/public"));
-app.set('views', __dirname);
-app.set('view engine', 'ejs');
-
-app.get('/', function(req, res) {
-    res.render('index', { layout: false });
-});
-
-server.on('request', app);
 
 server.listen(3000, function () {
     console.log('Listening on ' + server.address().port);
@@ -25,33 +13,43 @@ var wsServer = new WebSocketServer({
     httpServer: server,
 });
 
+var dataEmitter = EventEmitter({});
 var sentData = {};
+
+pg.connect(connString, function (error, client) {
+  if (error) {
+    console.log(error);
+  }
+
+  client.on('notification', function (msg) {
+    var parts = msg.payload.split(':');
+    var data = {
+      type: parts[0],
+      value: parts[1],
+    };
+
+    if (data.type in sentData && sentData[data.type] == data.value) {
+      return;
+    }
+
+    dataEmitter.emit('data', data);
+    sentData[data.type] = data.value;
+  });
+
+  client.query("LISTEN sensor_change");
+});
 
 wsServer.on('request', function (request) {
   var connection = request.accept('sensor-change', request.origin);
+  var data;
 
-  pg.connect(connString, function (error, client) {
-    if (error) {
-      console.log(error);
-    }
+  for (type in sentData) {
+    data = {type: type, value: sentData[type]};
+    connection.sendUTF(JSON.stringify(data));
+  }
 
-    client.on('notification', function (msg) {
-      var parts = msg.payload.split(':');
-      var data = {
-        type: parts[0],
-        value: parts[1],
-      };
-
-      if (data.type in sentData && sentData[data.type] == data.value) {
-        return;
-      }
-
-      connection.sendUTF(JSON.stringify({data: data}));
-
-      sentData[data.type] = data.value;
-    });
-
-    client.query("LISTEN sensor_change");
+  dataEmitter.on('data', function (data) {
+    connection.sendUTF(JSON.stringify(data));
   });
 });
 
