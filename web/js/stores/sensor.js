859646a605366ff577c9143994cf27173f13e51c
Stian Prestholdt <stian@e5r.no>
2016-06-28 22:06:22 +0200

    WIP sensor test data

--- web/js/stores/sensor.js
+++ web/js/stores/sensor.js
@@ -2,8 +2,46 @@ import EventEmitter from 'event-emitter';
 
 const Sensor = function (url, protocol) {
   this.socket = new WebSocket(url, protocol);
-  this.socket.onmessage = this.handleMessage.bind(this);
-  this.socket.onclose = this.handleOnClose.bind(this);
+  //this.socket.onmessage = this.handleMessage.bind(this);
+  //this.socket.onclose = this.handleOnClose.bind(this);
+// 
+  setInterval(() => {
+    const value = Math.floor(Math.random() * 35) + -35;
+    const message = '{"type": "temperature", "value": '+ value + '}';
+    this.handleMessage({data: message})
+  }, 1200);
+
+
+  setInterval(() => {
+    const speed = Math.floor(Math.random() * 20) + 0;
+    const gust = Math.floor(Math.random() * 20) + 0;
+    const message = '{"type": "wind", "value": "'+ speed + ','+gust+'"}';
+    this.handleMessage({data: message})
+  }, 1400);
+
+  setInterval(() => {
+    const value = Math.floor(Math.random() * 360) + 0;
+    const message = '{"type": "direction", "value": '+ value + '}';
+    this.handleMessage({data: message})
+  }, 1600);
+
+  setInterval(() => {
+    const value = Math.floor(Math.random() * 100) + 0;
+    const message = '{"type": "humidity", "value": '+ value + '}';
+    this.handleMessage({data: message})
+  }, 1800);
+
+  setInterval(() => {
+    const value = Math.floor(Math.random() * 20) + 0;
+    const message = '{"type": "precipitation", "value": '+ value + '}';
+    this.handleMessage({data: message})
+  }, 1800);
+
+  setInterval(() => {
+    const value = Math.floor(Math.random() * 1100) + 200;
+    const message = '{"type": "pressure", "value": '+ value + '}';
+    this.handleMessage({data: message})
+  }, 1800);
 };
 
 Sensor.prototype = {
