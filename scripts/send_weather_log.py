19ee869fd85106b18f2d56ed8d225f2b5cc7f02f
Stian Prestholdt <stian@e5r.no>
2016-07-13 15:36:54 +0200

    Add last sent id to send weather script

--- scripts/send_weather_log.py
+++ scripts/send_weather_log.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
 import requests
+import sys
+filepath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.realpath(filepath + '/..'))
 
 from database import models, session
 
@@ -23,12 +27,19 @@ def get_weather_data():
 
 def main():
     data = []
+    weather_sent = models.WeatherSent()
+
     for weather in get_weather_data():
         data.append(weather.to_dict())
+        last_id = weather.id
 
     response = requests.post(ENDPOINT_URL, data=data)
     if response.status_code == requests.codes.created:
-        print "ok! weathers stored"
+        weather_sent.last_sent_id = last_id
+        session.add(weather_sent)
+        session.commit()
+
+        print "ok! weathers sent and last id stored"
     else:
         response.raise_for_status()
 
